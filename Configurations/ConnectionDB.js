5c6108ef84a893326c32e750f90e5c9fca05182b
aligamal <aligamal@3ddx.com>
2020-05-09 15:08:37 +0200

    login added

--- Configurations/ConnectionDB.js
+++ Configurations/ConnectionDB.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 try
 {
-  const URL = "mongodb://localhost/marketDB" 
-  //const URL = "mongodb+srv://ali95880:08081995ali@cluster0-1bit0.mongodb.net/marketDB?retryWrites=true&w=majority";
+  //const URL = "mongodb://localhost/marketDB" 
+  const URL = "mongodb+srv://ali95880:08081995ali@cluster0-1bit0.mongodb.net/marketDB?retryWrites=true&w=majority";
  
    mongoose.connect( URL , {
    useCreateIndex:true ,
